f7b8a5ef1c20171f4c0ca41e7bf29ec7e6bd2029
glasscar46 <glasscar46@gmail.com>
2021-04-13 22:27:28 -0300

    changed intercala() structure

--- MergeSort/intercala.c
+++ MergeSort/intercala.c
@@ -7,7 +7,7 @@
 #pragma pack(1)
 
 typedef struct _Endereco Endereco;
-
+ FILE *saida;
 struct _Endereco
 {
 	char logradouro[72];
@@ -23,12 +23,13 @@ int compara(const void *e1, const void *e2)
 {
 	return strncmp(((Endereco*)e1)->cep,((Endereco*)e2)->cep,8);
 }
-void intercala(char *fileA, char *fileB,char * fileC);
+void intercala(FILE *a, char *fileB);
 
 int main(int argc, char** argv)
 {
     FILE *f,cepA,cepB;
     Endereco *e;
+    saida = fopen("saida.dat","rw");
     long posicao, qtd;
     f = fopen("cep.dat","r");
     fseek(f,0,SEEK_END);
@@ -52,20 +53,21 @@ int main(int argc, char** argv)
             fclose(aux);
             aux = NULL;
     }
-    FILE *saida = saidas[0];
-    for(int i = 1; i < 16; i++){
-        
-
+    for(int i = 0; i < 16; i++){
+        FILE *aux = saida;
+        char * dat = ".dat";
+        char * right = "cep_" + i ;
+        strcat(right,dat);
+        intercala(aux,right);
+        saida = fopen("saida.dat","rw");
     }
+    fclose(saida);
 
 }
- void intercala(char * fileA, char * fileB, char * fileC){
-     	FILE *a, *b, *saida;
+ void intercala(FILE  *a, char * fileB){
+     	FILE *b;
 	Endereco ea, eb;
-    a = fopen(fileA,"r");
 	b = fopen(fileB,"r");
-	saida = fopen(fileC,"w");
-    
 	fread(&ea,sizeof(Endereco),1,a);
 	fread(&eb,sizeof(Endereco),1,b);
 
